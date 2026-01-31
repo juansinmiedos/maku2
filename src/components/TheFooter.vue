4395c6dd8a2dcbbdac0a83ffb65e0d8360ba6256
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-01-31 16:14:31 -0600

    fix footer for medium size

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -5,7 +5,7 @@
         <div class="title-container">
           <div class="text-wrapper">
             <p class="title-lg">Don’t wait to get noticed.</p>
-            <h3 style="width: 100%; max-width: 355px;">The bold ones act first.</h3>
+            <h3>The bold ones act first.</h3>
           </div>
           
            <TheButton>Enter Your Email</TheButton>
@@ -14,18 +14,18 @@
 
       <div class="body-section">
         <div class="body-container">
-          <p class="overline-md uppercase text-neutral-500">Our approach to your brand?</p>
+          <p class="uppercase text-neutral-500" :class="windowIsMedium ? 'overline-sm' : 'overline-md'">Our approach to your brand?</p>
           <p class="title-md">Ask for a free 15-minute audit. We’ll review your current communication and tell you what’s getting in your way (and how to fix it).</p>
         </div>
         <div class="body-container">
-          <p class="overline-md uppercase text-neutral-500">Contact</p>
+          <p class="uppercase text-neutral-500" :class="windowIsMedium ? 'overline-sm' : 'overline-md'">Contact</p>
           <div class="contact-details">
             <a>michellepeniche@maku.agency</a>
             <a>+52 (62) 4320 0680</a>
           </div>
         </div>
         <div class="body-container">
-          <p class="overline-md uppercase text-neutral-500">Follow us</p>
+          <p class="uppercase text-neutral-500" :class="windowIsMedium ? 'overline-sm' : 'overline-md'">Follow us</p>
           <a>Instragram @maku.agency</a>
           <a>Tiktok</a>
         </div>
@@ -59,6 +59,8 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import TheButton from './TheButton.vue'
 
+const windowIsMedium = computed(() => window.innerWidth <= 830 && window.innerWidth > 380)
 </script>
