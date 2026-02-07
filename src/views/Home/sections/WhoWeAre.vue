0da77cc6e3605affa24bc67a54d40c8788765d9f
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-02-07 16:35:18 -0600

    Add styles for who we are for small width

--- src/views/Home/sections/WhoWeAre.vue
+++ src/views/Home/sections/WhoWeAre.vue
@@ -5,11 +5,13 @@
 
       <div class="body-wrapper">
         <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MAKU is a strategic branding and marketing agency that highlights the best of your brand.</h1>
+        <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MAKU is a strategic branding and marketing agency that highlights the best of your brand.</h4>
         <div class="body">
           <div class="body-container">
             <div>
               <h4>Through our proven ABCDS Method, we craft strategies and creative that help brands.</h4>
               <h3>Grow stronger</h3>
+              <h4 class="h4-until-small">Grow stronger</h4>
             </div>
 
             <TheButton>Learn more</TheButton>
