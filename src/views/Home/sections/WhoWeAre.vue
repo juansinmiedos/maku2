55369e9a92686f4d29898a03e643ad0c5fc22d6b
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-02-07 15:15:48 -0600

    Add styles for who we are for large and extra large width

--- src/views/Home/sections/WhoWeAre.vue
+++ src/views/Home/sections/WhoWeAre.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>who we are</div>
+  <section class="who-we-are-section">
+    <div class="container">
+      <p class="title title-lg">Who We Are</p>
+
+      <div class="body-wrapper">
+        <h1>&nbsp;&nbsp;&nbsp;&nbsp; MAKU is a strategic branding and marketing agency that highlights the best of your brand.</h1>
+        <div class="body">
+          <div class="body-container">
+            <div>
+              <h4>Through our proven ABCDS Method, we craft strategies and creative that help brands.</h4>
+              <h3>Grow stronger</h3>
+            </div>
+
+            <TheButton>Learn more</TheButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script setup>
+import TheButton from '@/components/TheButton.vue'
+
+</script>
