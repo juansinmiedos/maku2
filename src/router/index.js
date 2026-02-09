0678704caff29be877e3e762b65bb9716aa3c805
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-02-08 18:45:23 -0600

    Add empty views to test navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const loadComponent = component => () => import(`../views/Home/${component}.vue`)
+const loadHomeView = () => () => import(`../views/Home/HomeView.vue`)
+const loadAboutUsView = () => () => import(`../views/AboutUs/AboutUsView.vue`)
+const loadServicesView = () => () => import(`../views/Services/ServicesView.vue`)
+const loadProjectsView = () => () => import(`../views/Projects/ProjectsView.vue`)
+const loadContactView = () => () => import(`../views/Contact/ContactView.vue`)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: loadComponent("HomeView"),
+      path: "/",
+      name: "Home",
+      component: loadHomeView(),
+    },
+    {
+      path: "/about-us",
+      name: "AboutUs",
+      component: loadAboutUsView()
+    },
+    {
+      path: "/services",
+      name: "Services",
+      component: loadServicesView()
+    },
+    {
+      path: "/projects",
+      name: "Projects",
+      component: loadProjectsView()
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      component: loadContactView()
     },
   ],
 })
