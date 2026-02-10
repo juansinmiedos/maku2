c3297534b9836151c334005a022db41dbdb8e558
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-02-10 15:19:12 -0600

    Add basic ui for symbols section

--- src/views/AboutUs/sections/Symbols.vue
+++ src/views/AboutUs/sections/Symbols.vue
@@ -1,3 +1,18 @@
 <template>
-  <div></div>
+  <section class="symbols-section">
+    <div class="container">
+      <p class="title-lg">Long before brands existed,<br />people left marks.</p>
+
+      <div class="body-wrapper">
+        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Symbols carved into stone. Signs painted on walls. Traces that remained long after the moment passed.</h1>
+
+        <div class="body">
+          <div class="body-container">
+            <p class="title-md">We believe branding and marketing truly matter when they help a brand connect, stand out, and perform better as a business. </p>
+            <p class="p-md">At the core, the question is simple: why should people care about your brand? Our role is to help brands find that answer, and build the strategy to make it clear, relevant, and consistent.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
