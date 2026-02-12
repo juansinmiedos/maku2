9eed6d5ea75a8b4d83d7d602f5693552e841caa7
juansinmiedos <juan@montebit.io>
2026-02-12 11:15:38 -0600

    Add styles fro large size for symbols section

--- src/views/AboutUs/sections/Symbols.vue
+++ src/views/AboutUs/sections/Symbols.vue
@@ -4,7 +4,7 @@
       <p class="title-lg">Long before brands existed,<br />people left marks.</p>
 
       <div class="body-wrapper">
-        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Symbols carved into stone. Signs painted on walls. Traces that remained long after the moment passed.</h1>
+        <h1>Symbols carved into stone. Signs painted on walls. Traces that remained long after the moment passed.</h1>
 
         <div class="body">
           <div class="body-container">
