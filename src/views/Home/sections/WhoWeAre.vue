5a59d67f10aa4afb4cd5ca38755b9aa984d2d4a4
juansinmiedos <juan@montebit.io>
2026-02-12 09:23:14 -0600

    Add lottie, update min width size

--- src/views/Home/sections/WhoWeAre.vue
+++ src/views/Home/sections/WhoWeAre.vue
@@ -10,8 +10,9 @@
           <div class="body-container">
             <div>
               <h4>Through our proven ABCDS Method, we craft strategies and creative that help brands.</h4>
-              <h3>Grow stronger</h3>
-              <h4 class="h4-until-small">Grow stronger</h4>
+              <div ref="growStronger" class="lottie"></div>
+              <!-- <h3>Grow stronger</h3> -->
+              <!-- <h4 class="h4-until-small">Grow stronger</h4> -->
             </div>
 
             <TheButton>Learn more</TheButton>
@@ -23,6 +24,27 @@
 </template>
 
 <script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue"
+import lottie from "lottie-web"
+import animationData from "@/assets/grow-stronger.json"
+
 import TheButton from '@/components/atoms/TheButton.vue'
 
+const growStronger = ref(null)
+let animInstance = null
+
+onMounted(() => {
+  console.log(animationData)
+  animInstance = lottie.loadAnimation({
+    container: growStronger.value,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    animationData
+  })
+})
+
+onBeforeUnmount(() => {
+  animInstance?.destroy()
+});
 </script>
