b86a40f3f98f8092ad55f338de6ccc0d9f4abb61
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-02-21 13:13:44 -0600

    add first contact styles

--- src/views/Contact/ContactView.vue
+++ src/views/Contact/ContactView.vue
@@ -1,1 +1,14 @@
-<template>contact</template>
+<template>
+  <section class="contact-section">
+    <div class="box">
+      <!-- stepper -->
+
+      <div class="flex column" style="gap: 12px;">
+        <h4>The bold ones act first.</h4>
+        <p class="title-sm">Hi there! We’re excited to connect. To help us hit the ground running and tailor our strategy for maximum impact, please fill out the form below.</p>
+      </div>
+
+      <!-- form -->
+    </div>
+  </section>
+</template>
