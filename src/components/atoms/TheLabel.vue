3d44cc02a08c434ecd7698577c5fe8753b012441
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-02-22 18:40:10 -0600

    create label and added to projects view

--- src/components/atoms/TheLabel.vue
+++ src/components/atoms/TheLabel.vue
@@ -1,4 +1,20 @@
 <template>
-  <div>label
+  <div class="label" :class="{ active: isActive }" @click="handleClick">
+    <p class="title-sm">
+      <slot></slot>
+    </p>
   </div>
 </template>
+
+<script setup>
+const emit = defineEmits([ "click" ])
+
+const props = defineProps({
+  isActive: Boolean,
+  value: String,
+})
+
+function handleClick() {
+  emit("click", props.value)
+}
+</script>
--- src/views/Projects/ProjectsView.vue
+++ src/views/Projects/ProjectsView.vue
@@ -4,18 +4,31 @@
       <div class="container">
         <p class="title-lg">Long before brands existed,<br />people left marks.</p>
 
-        <div class="controls">
-          <h2>Our Projects</h2>
+        <div class="labels-section">
+          <div class="top">
+            <h2>Our Projects</h2>
+  
+            <div class="container">
+              <TheLabel :isActive="state.selectedLabels.length === 0" @click="state.selectedLabels = []">All</TheLabel>
 
-          <div>
-            <TheLabel>Botón!</TheLabel>
+              <TheLabel
+                v-for="(label, i) in state.labelOptions"
+                :key="i"
+                :value="label"
+                :isActive="state.selectedLabels.includes(label)"
+                @click="toggleLabelSelection"
+              >{{ label }}</TheLabel>
+            </div>
           </div>
+
+          <div class="bottom"></div>
         </div>
 
-        <div></div>
+        <div class="view-control">
+          b
+        </div>
       </div>
     </div>
-    <!-- head -->
 
     <!-- body -->
     <div class="projects-container"></div>
@@ -23,6 +36,41 @@
 </template>
 
 <script setup>
+import { reactive } from 'vue'
+
 import TheLabel from '@/components/atoms/TheLabel.vue'
-import TheButton from '@/components/atoms/TheButton.vue'
+// import TheButton from '@/components/atoms/TheButton.vue'
+
+const state = reactive({
+  labelOptions: [
+    "Brand Storytelling",
+    "Naming & Concept Creation",
+    "Brand Strategy",
+    "Pitch Decks",
+    "Full Brand Systems",
+    "Visual Identity Design",
+    "Brand Books",
+    "Packaging & Collaterals",
+    "Social Media Strategy",
+    "Content Creation",
+    "Creative & Digital Campaigns",
+    "AI-Driven Campaigns",
+    "Paid Advertising (Meta, Google, YouTube)",
+    "Launch Campaigns",
+    "Website & Funnel Building",
+    "UX/UI Strategy",
+    "Copywriting for Web & Digital",
+  ],
+
+  selectedLabels: [],
+})
+
+function toggleLabelSelection(value) {
+  if (state.selectedLabels.includes(value)) {
+    const index = state.selectedLabels.findIndex(item => item === value)
+    state.selectedLabels.splice(index, 1)
+  } else {
+    state.selectedLabels.push(value)
+  }
+}
 </script>
