b3323f6ae51dbc123a8639330af9905f5a4c9b8f
juansinmiedos <juan@montebit.io>
2026-02-27 14:55:05 -0600

    feat add new hero json

--- src/views/AboutUs/sections/Hero.vue
+++ src/views/AboutUs/sections/Hero.vue
@@ -1,3 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <div ref="homeHero" class=""></div>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue"
+import lottie from "lottie-web"
+import animationData from "@/assets/about-hero.json"
+
+const homeHero = ref(null)
+
+let animInstance = null
+
+onMounted(() => {
+  animInstance = lottie.loadAnimation({
+    container: homeHero.value,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    animationData
+  })
+})
+
+onBeforeUnmount(() => {
+  animInstance?.destroy()
+})
+</script>
--- src/views/Home/sections/Hero.vue
+++ src/views/Home/sections/Hero.vue
@@ -1,7 +1,31 @@
 <template>
   <section class="hero">
+    <div ref="homeHero" class=""></div>
+    
     <img src="../../../assets/maku-logo.svg" alt="logo" />
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue"
+import lottie from "lottie-web"
+import animationData from "@/assets/home-hero.json"
+
+const homeHero = ref(null)
+
+let animInstance = null
+
+onMounted(() => {
+  animInstance = lottie.loadAnimation({
+    container: homeHero.value,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    animationData
+  })
+})
+
+onBeforeUnmount(() => {
+  animInstance?.destroy()
+})
+</script>
