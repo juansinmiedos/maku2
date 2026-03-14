622a87d0600098301da1967fab379b83bcd34c4a
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-03-14 16:19:31 -0600

    Add dot env for base url

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseURL = "http://localhost:3000/api"
+const baseURL = import.meta.env.VITE_API_BASE_URL
 const headers = {
   "Content-Type": "application/json",
 }
