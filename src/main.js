f163aba252be6d80c7b1d0c10285581837640ef8
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-03-20 19:07:30 -0600

    install vuetify

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia"
 import App from './App.vue'
 import router from './router'
+import { registerPlugins } from "./plugins"
 
 const app = createApp(App)
 const pinia = createPinia()
 
+registerPlugins(app)
 app.use(router).use(pinia)
 
 app.mount('#app')
